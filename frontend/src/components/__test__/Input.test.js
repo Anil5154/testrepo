848c9c6b55682613114a90dbb57f1e9879d8faab
Anil Etipogu <anile@conceptvines.com>
2024-11-07 23:19:34 +0530

    Change-32

--- frontend/src/components/__test__/Input.test.js
+++ frontend/src/components/__test__/Input.test.js
@@ -11,7 +11,6 @@ describe('Input Component', () => {
 
     fireEvent.change(inputField, { target: { value: 'Hello, World!' } });
     expect(inputField.value).toBe('Hello, World!');
-	
-    expect(screen.getByText('Hello, World!')).toBeInTheDocument();
+	expect(screen.getByText('Hello, World!')).toBeInTheDocument();
   });
 });
